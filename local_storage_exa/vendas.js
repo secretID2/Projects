d5d150484542ba2a283fbb73b349d9f1800b1f8e
secretid2 <secretidentity009@gmail.com>
2016-05-28 22:40:01 +0100

    Vendas

---
+++ local_storage_exa/vendas.js
@@ -0,0 +1,15 @@
+
+
+
+window.onload = function init() {
+	var button1=document.getElementById("b1");
+	button1.addEventListener("click",Salgados,false);
+	var b2=document.getElementById("b2");
+	b2.addEventListener("click",my_function,false);
+	
+}
+
+function Salgados(){
+	
+	
+}
